a3b664d65b45058d6db7f114b10a639c38082223
Ilya Kravchenko <iliakravchenko09@gmail.com>
2025-02-27 10:46:23 +0500

    fix

--- sanity/client.ts
+++ sanity/client.ts
@@ -2,10 +2,12 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  token: process.env.SANITY_API_TOKEN,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tbrctr9r",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-02-26",
+  token:
+    process.env.SANITY_API_TOKEN ||
+    "skL86jOpT2INKDJipXHjtLFw3UhfVMBTMYWdKz3rhwi3hIGcUr8zff0LIBSWDunolBiAx0jm1vWb0befDiaBYBWKDdIdfCtUCBl9DyVQc9YOTqDF5Foo2XyvxPzF4VO4nf4sPeJqMB76qV8LXTYB1cUo3f2ETE1KIz8spYBJo0PXnXVIbm3s",
   useCdn: false,
 });
 
