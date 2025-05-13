53563c156aba47f97e2ba7955e6f5afb6e2d739c
Ilya Kravchenko <iliakravchenko09@gmail.com>
2025-05-13 17:18:06 +0500

    fdfd

--- components/welcome-flow.tsx
+++ components/welcome-flow.tsx
@@ -36,20 +36,44 @@ export function WelcomeFlow() {
     <>
       {/* Переключатель языка */}
       <div className="absolute top-4 right-4 flex gap-2">
-        <button className="bg-gray-700 text-white px-3 py-1 rounded-lg shadow-md hover:bg-gray-800" onClick={() => handleLanguageChange('ru')}>RU</button>
-        <button className="bg-gray-700 text-white px-3 py-1 rounded-lg shadow-md hover:bg-gray-800" onClick={() => handleLanguageChange('kk')}>KK</button>
+        <button
+          className="bg-gray-700 text-white px-3 py-1 rounded-lg shadow-md hover:bg-gray-800"
+          onClick={() => handleLanguageChange("ru")}
+        >
+          RU
+        </button>
+        <button
+          className="bg-gray-700 text-white px-3 py-1 rounded-lg shadow-md hover:bg-gray-800"
+          onClick={() => handleLanguageChange("kk")}
+        >
+          KK
+        </button>
       </div>
 
       {/* Приветственное окно */}
       {step === 1 && (
         <Dialog open>
-          <DialogContent hideClose  className="bg-gradient-to-r from-blue-500 to-purple-600 text-white p-6 rounded-lg shadow-xl">
+          <DialogContent
+            hideClose
+            className="bg-gradient-to-r from-blue-500 to-purple-600 text-white p-6 rounded-lg shadow-xl"
+          >
             <div className="text-center">
-              <Image src="./preview.png" alt="Ак Кайын" width={150} height={150} className="mx-auto mb-4" />
-              <h2 className="text-4xl font-extrabold drop-shadow-lg">{t("welcome")}</h2>
+              <Image
+                src="./preview.png"
+                alt="Ак Кайын"
+                width={150}
+                height={150}
+                className="mx-auto mb-4"
+              />
+              <h2 className="text-4xl font-extrabold drop-shadow-lg">
+                {t("welcome")}
+              </h2>
               <p className="mt-4 text-lg">{t("welcomeMessage")}</p>
               <p className="text-[#d8d0d5] mt-4 mb-4"> {t("language")} </p>
-              <Select onValueChange={handleLanguageChange} defaultValue={i18n.language}>
+              <Select
+                onValueChange={handleLanguageChange}
+                defaultValue={i18n.language}
+              >
                 <SelectTrigger className="w-[140px] mx-auto border-green-500 text-green-700 font-semibold">
                   <SelectValue />
                 </SelectTrigger>
@@ -59,7 +83,12 @@ export function WelcomeFlow() {
                   <SelectItem value="en">English</SelectItem>
                 </SelectContent>
               </Select>
-              <Button className="mt-4 bg-white text-blue-600 font-semibold px-6 py-2 rounded-lg shadow-md hover:bg-blue-100" onClick={() => setStep(2)}>{t("continue")}</Button>
+              <Button
+                className="mt-4 bg-white text-blue-600 font-semibold px-6 py-2 rounded-lg shadow-md hover:bg-blue-100"
+                onClick={() => setStep(2)}
+              >
+                {t("continue")}
+              </Button>
             </div>
           </DialogContent>
         </Dialog>
@@ -76,12 +105,27 @@ export function WelcomeFlow() {
               <li>{t("menuOrder")}</li>
               <li> {t("serviceCharge")}</li>
               <li>{t("rentalInfo")}</li>
-              <li className="flex items-center gap-2 flex-wrap"> {t('dishBreakage')}</li>
+              <li className="flex items-center gap-2 flex-wrap">
+                {" "}
+                {t("dishBreakage")}
+              </li>
             </ul>
-            <p className="text-lg font-bold text-red-500 mt-2">{t("agreement")}</p>
+            <p className="text-lg font-bold text-red-500 mt-2">
+              {t("agreement")}
+            </p>
             <div className="flex gap-4 justify-center mt-4">
-              <Button className="bg-green-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-600" onClick={() => setStep(3)}>{t("yes")}</Button>
-              <Button className="bg-black text-white px-6 py-2 rounded-lg shadow-md hover:bg-gray-700" onClick={reloadPage}>{t("no")}</Button>
+              <Button
+                className="bg-green-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-600"
+                onClick={() => setStep(3)}
+              >
+                {t("yes")}
+              </Button>
+              <Button
+                className="bg-black text-white px-6 py-2 rounded-lg shadow-md hover:bg-gray-700"
+                onClick={reloadPage}
+              >
+                {t("no")}
+              </Button>
             </div>
           </DialogContent>
         </Dialog>
@@ -89,39 +133,69 @@ export function WelcomeFlow() {
 
       {/* Окно с ценами на аренду */}
       {step === 3 && (
-  <Dialog open>
-  <DialogContent className="bg-gray-900 text-white p-4 md:p-6 rounded-lg shadow-xl border border-gray-700 animate-fadeIn max-w-[90vw] sm:max-w-md md:max-w-lg lg:max-w-xl max-h-[80vh] overflow-y-auto">
+        <Dialog open>
+          <DialogContent className="bg-gray-900 text-white p-4 md:p-6 rounded-lg shadow-xl border border-gray-700 animate-fadeIn max-w-[90vw] sm:max-w-md md:max-w-lg lg:max-w-xl max-h-[80vh] overflow-y-auto">
+            <h2 className="text-2xl md:text-3xl font-extrabold text-yellow-400 flex items-center gap-2">
+              🏠 {t("rent")}
+            </h2>
 
-      <h2 className="text-2xl md:text-3xl font-extrabold text-yellow-400 flex items-center gap-2">
-        🏠 {t("rent")}
-      </h2>
-
-      <ul className="mt-2 space-y-2 md:space-y-3 text-sm md:text-base">
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-red-500">♨️</span> {t('grillHouse')} </li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-blue-500">🎤</span> {t('karaokeDeposit')} </li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-cyan-400">🔥</span> {t('sauna')} </li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-gray-400">🛖</span> {t('smallTapchan')}</li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-green-500">🛖</span> {t('mediumTapchan')}</li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-red-400">🎪</span> {t('bigGazebo')}</li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-orange-400">VIP</span> {t('vipGazebo')}</li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-pink-400">VIP</span> {t('vipCabin')}</li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-yellow-500">VIP</span> {t('vipHall')}</li>
-      <li className="flex items-center gap-2 flex-wrap"><span className="text-purple-500">VIP</span> {t('vipTapchan')} </li>
-
-    </ul>
-      <div className="flex flex-col sm:flex-row gap-4 justify-center mt-4 md:mt-4">
-      <p className="text-[#8B5A2B] text-base md:text-lg mb-4 md:mb-6 italic text-center">{t("agree")}</p>
-        <Button className="bg-green-500 text-white px-4 md:px-6 py-2 rounded-lg shadow-md hover:bg-green-600 transition-transform transform hover:scale-105 w-full sm:w-auto" onClick={() => setStep(4)}>
-          {t("yes")}
-        </Button>
-        <Button className="bg-gray-700 text-white px-4 md:px-6 py-2 rounded-lg shadow-md hover:bg-gray-800 transition-transform transform hover:scale-105 w-full sm:w-auto" onClick={reloadPage}>
-          {t("no")}
-        </Button>
-      </div>
-    </DialogContent>
-  </Dialog>
-
-)}
+            <ul className="mt-2 space-y-2 md:space-y-3 text-sm md:text-base">
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-red-500">♨️</span> {t("grillHouse")}{" "}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-blue-500">🎤</span>{" "}
+                {t("karaokeDeposit")}{" "}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-cyan-400">🔥</span> {t("sauna")}{" "}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-gray-400">🛖</span> {t("smallTapchan")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-green-500">🛖</span> {t("mediumTapchan")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-red-400">🎪</span> {t("bigGazebo")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-orange-400">VIP</span> {t("vipGazebosr")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-orange-400">VIP</span> {t("vipGazebo")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-pink-400">VIP</span> {t("vipCabin")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-yellow-500">VIP</span> {t("vipHall")}
+              </li>
+              <li className="flex items-center gap-2 flex-wrap">
+                <span className="text-purple-500">VIP</span>{" "}
+                {t("vipTapchan")}{" "}
+              </li>
+            </ul>
+            <div className="flex flex-col sm:flex-row gap-4 justify-center mt-4 md:mt-4">
+              <p className="text-[#8B5A2B] text-base md:text-lg mb-4 md:mb-6 italic text-center">
+                {t("agree")}
+              </p>
+              <Button
+                className="bg-green-500 text-white px-4 md:px-6 py-2 rounded-lg shadow-md hover:bg-green-600 transition-transform transform hover:scale-105 w-full sm:w-auto"
+                onClick={() => setStep(4)}
+              >
+                {t("yes")}
+              </Button>
+              <Button
+                className="bg-gray-700 text-white px-4 md:px-6 py-2 rounded-lg shadow-md hover:bg-gray-800 transition-transform transform hover:scale-105 w-full sm:w-auto"
+                onClick={reloadPage}
+              >
+                {t("no")}
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      )}
     </>
   );
 }
